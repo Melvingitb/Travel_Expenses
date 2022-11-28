9ebfce48be82b197a20d9c7f9d698c5ca814dbc4
Melvin <melvingit@gmail.com>
2022-11-28 01:56:48 -0800

    create main and header file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    double total = 0;
+    int days = 0;
+    int arrival = 0;
+    int departure = 0;
+
+    printf("How many days total were spent on the trip?");
+
+}
