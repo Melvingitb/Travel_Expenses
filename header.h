fe692cfd470bf0712d104bfeb80a2e90d2eea690
Melvin <melvingit@gmail.com>
2022-11-29 01:41:45 -0800

    added more functions to header

--- header.h
+++ header.h
@@ -1,5 +1,7 @@
 double getAllowable(int days, int arrival, int departure);
-double getTransport(int days);
+double getTransport(int days);  //round-trip airfare, car rentals, miles driven if private vehicle was used($0.27 per mile)
+double getPublic(int days); //parking fees ($6 per day allowed), taxi fees ($10 per day allowed), conference or seminar registration fees
+double getLiving(int days, int arrival, int departure); //hotel expenses ($90 per day allowed), amount of each meal eaten
 
 //in time.c
 int getDays();
