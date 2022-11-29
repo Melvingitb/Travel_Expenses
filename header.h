0bb8a20f8221c39426d88d798f8be59c91787bff
Melvin <melvingit@gmail.com>
2022-11-29 01:12:41 -0800

    add more to main, header, and create time

---
+++ header.h
@@ -0,0 +1,7 @@
+double getAllowable(int days, int arrival, int departure);
+double getTransport(int days);
+
+//in time.c
+int getDays();
+int getDeparture();
+int getArrival();
--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include "header.h"
 
 int main(){
     double total = 0;
     int days = 0;
-    int arrival = 0;
     int departure = 0;
+    int arrival = 0;
+    double allowable = 0;
+    double parking = 0;
+    double food = 0;
 
-    printf("How many days total were spent on the trip?");
+    days = getDays();
+    departure = getDeparture();
+    arrival = getArrival();
+
+    printf("%d %d", departure, arrival);
 
 }
