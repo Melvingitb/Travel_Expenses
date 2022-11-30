d7afc8581b2357b8aef46340abd25f092f40c72d
wfgarcia <112603956+wfgarcia@users.noreply.github.com>
2022-11-29 21:32:58 -0800

    Add files via upload
    initial final display and results for travel expenses.

---
+++ display.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//final variables declare
+float final; 
+
+//final print of all parts
+void getDisplay(double total, double allowable, double expense){
+
+    printf("Total expenses incurred: %d\n", &total);
+
+    printf("Total allowable expenses: %d\n", &allowable);
+
+    final = total - expsnse;
+
+    if(final > allowable){
+        printf("You owe: %d\n", &final);
+    }else{
+        printf("You saved: %d\n", &final);
+    }
+}
