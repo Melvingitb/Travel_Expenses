3b63742675075e1b7e6c355637a7f39cbedcb717
unknown <sanders.aidan.c.ss@gmail.com>
2022-11-29 17:37:21 -0800

    Added return variable and scanned input

--- PublicTransitFees.c
+++ PublicTransitFees.c
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <stdio.h>
 
 double getPublic(int days){
+  double totalExpense = 0;
+  double input;
+  
   for (int i = 0; i < days; i++){
-    
+    printf("Enter fee for day %d", (i + 1));
+
+    scanf("%lf", &input);
   }
+
+  return totalExpense;
 }
