4aa1b8942ec68e416cc4b42926a4c78d2c9ad0d5
unknown <sanders.aidan.c.ss@gmail.com>
2022-11-29 19:47:10 -0800

    compeleted the curent form of fee calcutlation

--- PublicTransitFees.c
+++ PublicTransitFees.c
@@ -3,11 +3,26 @@
 double getPublic(int days){
   double totalExpense = 0;
   double input;
+  //double feeAccumulator = 0;
   
   for (int i = 0; i < days; i++){
-    printf("Enter fee for day %d", (i + 1));
+    printf("Enter the taxi fee for day %d", (i + 1));
 
     scanf("%lf", &input);
+
+    if (input > 10){
+      totalExpense += (input - 10);
+    }
+  }
+
+  for (int i = 0; i < days; i++){
+    printf("Enter the parking fee for day %d", (i + 1));
+
+    scanf("%lf", &input);
+
+    if (input > 6){
+      totalExpense += (input - 6);
+    }
   }
 
   return totalExpense;
