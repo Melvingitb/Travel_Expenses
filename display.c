6da13c87154b0168cd8fe43346d4af71e6685f7f
wfgarcia <112603956+wfgarcia@users.noreply.github.com>
2022-11-29 22:30:41 -0800

    changes to variables and logic

--- display.c
+++ display.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-//final variables declare
-float final; 
-
+//final variables declare 
+double owed = 0;
 //final print of all parts
 void getDisplay(double total, double allowable, double expense){
 
-    printf("Total expenses incurred: %d\n", &total);
+    printf("Total expenses incurred: %d\n", total);
 
-    printf("Total allowable expenses: %d\n", &allowable);
+    printf("Total allowable expenses: %d\n", allowable);
 
-    final = total - expense;
-
-    if(final > allowable){
-        printf("You owe: %d\n", &final);
+    if(expense > allowable){
+        owed = expense - allowable;
+        printf("You owe: %d\n", owed);
     }else{
-        printf("You saved: %d\n", &final);
+        printf("You saved: %d\n", expense);
     }
 }
